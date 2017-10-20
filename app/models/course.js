9d5203612d14f902e33cbc58cbcaf6b441cc086a
Couhp <phuocluu96@gmail.com>
2017-10-20 21:40:49 +0700

    nothing

--- app/models/course.js
+++ app/models/course.js
@@ -1,16 +1,16 @@
 const mongoose = require(`${r}/config/database/mongoose`)
 
 const { Schema } = mongoose;
-var coursesSchema = new Schema({
+var courseSchema = new Schema({
   name: String,
   description : String,
   topic :{
     type : Array,
     default: []
   },
-  level : Number
+  
 })
 
-var CoursesSchema = mongoose.model('Course', coursesSchema);
+var CourseSchema = mongoose.model('Course', courseSchema);
 
 module.exports = CoursesSchema;
