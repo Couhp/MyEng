7293c9ded982c6d2c8a1401cc29e8c11ae796507
Couhp <phuocluu96@gmail.com>
2017-10-30 22:52:00 +0700

    update model

--- app/models/fill_question.js
+++ app/models/fill_question.js
@@ -2,7 +2,8 @@ const mongoose = require(`${r}/config/database/mongoose`)
 const { Schema } = mongoose;
 const fillSchema= new Schema({
     quesion     : {type :String, required : true},
-    answer      : {type :String, required : true},
+    answer      : {type :Array, required : true},
+    // Answer is a list of accepted answer
     topic :{
 		type: Schema.Types.ObjectId,
 		ref: 'topic'
