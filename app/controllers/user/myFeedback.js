918b07c4e5800c09666fe4435d2436f1d65f5c7a
Pain247 <dangductungcfc@gmail.com>
2017-12-02 12:00:26 +0700

    fix feedback

--- app/controllers/user/myFeedback.js
+++ app/controllers/user/myFeedback.js
@@ -3,7 +3,7 @@ const Feedback = require(global.__base + '/app/models/feedback.js');
 const mongoose = require('mongoose');
 let myFeedback = (req, res) => {
     let id = new mongoose.Types.ObjectId(req.session.userId);
-    Feedback.findOne({ user: id }).exec((err, feedback) => {
+    Feedback.find({ user: id }).exec((err, feedback) => {
         if (err) return res.status(500).json({ errCode: 500, msg: "Internal error" });
         if (!feedback) return res.status(404).json({ errCode: 404, msg: "Not found" });
         else {
