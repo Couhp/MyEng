8870948e295dad4b6b29b7d9d0024f9f0da58d54
Pain247 <dangductungcfc@gmail.com>
2017-12-03 12:30:45 +0700

    fix feedback client

--- app/views/public/javascript/feedback.js
+++ app/views/public/javascript/feedback.js
@@ -1,15 +1,18 @@
-
-$(document).ready(function(){
-    
+$(document).ready(function() {
+    $("#newFeed").hide()
+    $("#create").on('click', function() {
+        $("#create").hide();
+        $("#newFeed").show();
+    });
     // ==== GLOBAL VARIABLE =========
     var userId = ""
 
 
-     // ================= ROUTING ============================
+    // ================= ROUTING ============================
 
-     $("#feedback").on('click', function() {
+    $("#feedback").on('click', function() {
         window.location = "http://localhost:8080/MyEng/FeedBack";
-    }) 
+    })
 
     $("#main").on('click', function() {
         window.location = "http://localhost:8080/MyEng/Main";
@@ -19,17 +22,33 @@ $(document).ready(function(){
     $("#sendFeedback").on('click', function() {
         var subject = $("#fb-subject").val()
         var content = $("#fb-content").val()
-        $.ajax({
-            type: "POST",
-            method: "POST",
-            url: "http://localhost:8080/api/user/feedback",
-            data: { "subject":subject, "content":content },
-            success: function(data) {
-                console.log(data)
-            }})
+        if (subject.length === 0 && content.length === 0) {
+            $("#fb-subject").css("border", "1px solid red");
+            $("#fb-content").css("border", "1px solid red");
+        } else if (subject.length === 0) {
+            $("#fb-subject").css("border", "1px solid red");
+            $("#fb-content").css("border", "1px solid black");
+        } else if (content.length === 0) {
+            $("#fb-subject").css("border", "1px solid black");
+            $("#fb-content").css("border", "1px solid red");
+        } else {
+            $.ajax({
+                type: "POST",
+                method: "POST",
+                url: "http://localhost:8080/api/user/feedback",
+                data: { "subject": subject, "content": content },
+                success: function(data) {
+                    if (data.errCode === 200) {
+                        alert(data.msg)
+                        $("#newFeed").hide()
+                        $("#create").show()
+                    }
+                }
+            })
+        }
     })
 
-    var getMyFb = function() {   
+    var getMyFb = function() {
         $.ajax({
             type: "GET",
             method: "GET",
@@ -37,7 +56,8 @@ $(document).ready(function(){
             data: {},
             success: function(data) {
                 showFb(2, data.data)
-            }})
+            }
+        })
     }
 
     var getAllFb = function() {
@@ -48,59 +68,78 @@ $(document).ready(function(){
             data: {},
             success: function(data) {
                 showFb(1, data.data)
-            }})
+            }
+        })
     }
 
     var showFb = function(type, data) {
         var divId = ""
-        if (type == 1) {divId = "#news-content"}
-        else divId = "#my-content"
+        if (type === 1) {
+            divId = "#news-content"
+        } else {
+            divId = "#my-content"
+        }
         if (data == null || data.length == 0) {
-            console.log("sdvlnsdvlksdnsvln")
             $(divId).append(getFbHTML("null"))
-        } 
-        else {data.forEach(element => {
-            var html = getFbHTML(element)
-            $(divId).append(html)
-        });}
+        } else {
+            data.forEach(element => {
+                var html = getFbHTML(element)
+                $(divId).append(html)
+            });
+        }
     }
-
-    var getFbHTML = function (data) {
+    var getUser = function(userid) {
+        let res = "";
+        $.ajax({
+            type: "POST",
+            method: "POST",
+            async: false,
+            url: "http://localhost:8080/api/user/getinfo",
+            data: { "userid": userid },
+            success: function(data) {
+                res = data.data.username
+            }
+        })
+        return res
+    }
+    var getFbHTML = function(data) {
         if (data == "null") {
-            var html = "<p><h3 class='bg-info'>Nothing New</h3></p>"
+            var html = "<div class='nothing bg-info'><h class='nothing-text'>Không có dữ liệu.</h></div><br/>"
             return html
         }
-        var subject =  (data.subject)!=null ? (data.subject) : "No title " 
+        var subject = (data.subject) != null ? (data.subject) : "No title "
         var content = data.content
         var reply = data.reply
+        if (reply === "") {
+            reply = "Không có trả lời"
+        }
         var html = "<div class='alert'>" +
-                "<h3 class='bg-warning'><b> " + subject +"</b></h3>" +
-                "<blockquote><p class='text-info'>" + content +"</p>" +
-                "<footer class='text-muted'>By " + "Null" + "</footer><br>" +
-                // "<p class='text-muted'>Reply</p>" + 
-                "<p class='text-primary'>  " + reply + "</p></blockquote><br>" + 
-                "</div>"
+            "<h4 class='bg-warning'><b><i class='fa fa-user' aria-hidden='true'>&nbsp;</i><a href='#' style='text-decoration: none;'><strong class='text-primary'> " + getUser(data.user) + "</strong></a>  : &nbsp;&nbsp;&nbsp;" + subject + "</b></h4>" +
+            "<blockquote><p class='text-info'>" + content + "</p>" +
+            // "<footer class='text-muted'>By " + getUser(data.user) + "</footer><br>" +
+            "<strong class='text-primary'> Trả lời : &nbsp;&nbsp;&nbsp;   </strong>   " + reply + "</blockquote><br>" +
+            "</div>"
         return html
     }
 
 
     // ==========   INFO ========================
     function setInfo(data) {
-        
-                function normalize(str) {
-                    if (str.indexOf("/") === 1) {
-                        let arr = str.split('/');
-                        arr.splice(0, 1);
-                        return '/' + arr.join('/');
-                    } else {
-                        let arr = str.split('\\');
-                        arr.splice(0, 1);
-                        return '/' + arr.join('/');
-                    }
-        
-                }
-                $("#avatar").attr("src", normalize(data.avatar));
-                $("#displayname").text(data.displayName);
+
+        function normalize(str) {
+            if (str.indexOf("/") === 1) {
+                let arr = str.split('/');
+                arr.splice(0, 1);
+                return '/' + arr.join('/');
+            } else {
+                let arr = str.split('\\');
+                arr.splice(0, 1);
+                return '/' + arr.join('/');
+            }
+
+        }
+        $("#avatar").attr("src", normalize(data.avatar));
+        $("#displayname").text(data.displayName);
     }
 
     var callInfo = function(callback) {
@@ -117,13 +156,13 @@ $(document).ready(function(){
         });
     }
 
-//=========================================
-    $(".nav-tabs a").click(function(){
+    //=========================================
+    $(".nav-tabs a").click(function() {
         $(this).tab('show');
     });
-    $('.nav-tabs a').on('shown.bs.tab', function(event){
-        var x = $(event.target).text();         // active tab
-        var y = $(event.relatedTarget).text();  // previous tab
+    $('.nav-tabs a').on('shown.bs.tab', function(event) {
+        var x = $(event.target).text(); // active tab
+        var y = $(event.relatedTarget).text(); // previous tab
         $(".act span").text(x);
         $(".prev span").text(y);
     });
@@ -134,8 +173,8 @@ $(document).ready(function(){
             getAllFb()
             getMyFb()
         })
-        
+
     }
-    
+
     main()
 });
